9621ea218d3699fa1a4ed8f2e9cc897b90434746
Ollie Edwards <olliee@mobbu.com>
2014-05-22 12:12:24 +0100

    Refactor task graph into more standard grunt format

--- tasks/graph_runner.js
+++ tasks/graph_runner.js
@@ -8,25 +8,27 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
+    var NAME = 'graph';
+    var PREFIX = NAME + ':';
 
     var Digraph = require("graphlib").Digraph;
-    var alg  = require("graphlib").alg;
+    var alg = require("graphlib").alg;
     var filter = require("graphlib").filter;
 
     var _ = require('lodash');
 
-    var taskGraph = new Digraph();
-
-    grunt.registerTask('run-graph', function() {
-        var tasksToRun = _.toArray(arguments);
-        prepareTaskGraph();
+    //Entry point for running a graph of tasks
+    grunt.registerTask('run-graph', function () {
+        var tasksToRun = getTasksToRun.apply(this, arguments);
+        var taskGraph = prepareTaskGraph();
 
         console.log("Root tasks to run ", tasksToRun);
         var allTasks = [];
-        _.each(tasksToRun, function(task) {
+        _.each(tasksToRun, function (task) {
             allTasks.push(task);
-            _.each(alg.dijkstra(taskGraph, task), function(val, key) {
+            _.each(alg.dijkstra(taskGraph, task), function (val, key) {
                 if (val.predecessor) {
                     allTasks.push(key);
                 }
@@ -38,24 +40,50 @@ module.exports = function(grunt) {
         grunt.task.run(computedTasks);
     });
 
-    grunt.graph = function(graph) {
-        _(graph || {}).each(function(node, name) {
-            taskGraph.addNode(name, node);
-        });
-    };
+    //Actual function run when a graph task is called
+    grunt.registerMultiTask(NAME, 'Tasks with interdependencies', function () {
+        var task = this.data.task;
+        if (task) {
+            if (typeof task === 'function') {
+                task.apply(this, arguments);
+            } else {
+                grunt.task.run(task);
+            }
+        }
+    });
 
-    var prepareTaskGraph = function() {
-        taskGraph.eachNode(function(name, node) {
-            _(node.dependencies || []).each( function(dep) {
-                if (taskGraph.hasNode(dep)) {
-                    taskGraph.addEdge(null, name, dep);
-                } else {
-                    console.error('no task %s was found whilst resolving dependencies of %s', dep, name);
-                }
+    //Build the directed graph based on the graph config block
+    var prepareTaskGraph = function () {
+        var taskGraph = new Digraph();
+        _.each(grunt.config('graph'), function (task, key) {
+            var qualifiedName = 'graph:' + key;
+            registerNode(taskGraph, qualifiedName);
+            _.each((task.dependencies || []), function (dep) {
+                //If there is a dependency on a task outside the graph then we need to add it to the directed graph
+                registerNode(taskGraph, dep);
+                taskGraph.addEdge(null, qualifiedName, dep);
             });
-
-            grunt.registerTask(name, node.action);
         });
+        return taskGraph;
     };
 
+    //Check if a node exists in the digraph, and if not add it
+    var registerNode = function (graph, key, node) {
+        if (!graph.hasNode(key)) {
+            graph.addNode(key, node);
+        }
+    };
+
+    /**
+     * Build initial list of root tasks, appending the prefix to any tasks which are missing it
+     * @returns {Array}
+     */
+    var getTasksToRun = function () {
+        return _.toArray(arguments).map(function (t) {
+            if (!t.indexOf(PREFIX) !== 0) {
+                t = PREFIX + t;
+            }
+            return t;
+        });
+    }
 };
